3a02499308ff2a815c902badca535c7d969c7b3c
Daniel Nowak <danieln311@gmail.com>
2026-02-03 19:40:44 +0000

    code

--- script.js
+++ script.js
@@ -4,21 +4,42 @@ let choice3 = "paper"
 
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3) + 1;
-    let selection;
+    let computerSelection;
 
     switch (random) {
         case 1:
-            selection = choice1;
+            computerSelection = choice1;
             break;
         case 2:
-            selection = choice2;
+            computerSelection = choice2;
             break;
         case 3:
-            selection = choice3;
+            computerSelection = choice3;
             break;
     }
 
-    return selection;
+    return computerSelection;
 }
 
 console.log(getComputerChoice());
+
+function getPlayerChoice() {
+    let random = Math.floor(Math.random() * 3) + 1
+    let playerSelection;
+
+    switch (random) {
+        case 1:
+            computerSelection = choice1;
+            break;
+        case 2:
+            computerSelection = choice2;
+            break;
+        case 3:
+            computerSelection = choice3;
+            break;
+    }
+
+    return computerSelection;
+}
+
+console.log(getPlayerChoice());
