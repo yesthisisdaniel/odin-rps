73aeca727063d7e7216199ef17a40083fb48e596
Daniel Nowak <danieln311@gmail.com>
2026-02-04 13:20:36 +0000

    Create function to get player selection and create function to play one round

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice () {
     let scissors = "scissors";
     let paper = "paper";
@@ -16,4 +19,38 @@ function getComputerChoice () {
     }
 }
 
-console.log(getComputerChoice())
+function getPlayerChoice () {
+    let selection = prompt("What is your choice?");
+
+    return selection;
+}
+
+
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) {
+        console.log("It's a tie!")
+    }
+    else if (playerChoice == "rock" && computerChoice == "scissors") {
+        playerScore++;
+        console.log("You win! " + playerChoice + " beats " + computerChoice)
+    }
+    else if (playerChoice == "scissors" && computerChoice == "paper") {
+        playerScore++;
+        console.log("You win! " + playerChoice + " beats " + computerChoice)
+    }
+    else if (playerChoice == "paper" && computerChoice == "rock") {
+        playerScore++;
+        console.log("You win! " + playerChoice + " beats " + computerChoice)
+    }
+    else {
+        computerScore++;
+        console.log("You lose! " + computerChoice + " beats " + playerChoice)
+    }
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+playRound(playerSelection, computerSelection);
+console.log(playerScore, computerScore)
