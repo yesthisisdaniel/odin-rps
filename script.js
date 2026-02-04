e266aa42eb92aa8cf21789ed7e5974cf95a1274c
Daniel Nowak <danieln311@gmail.com>
2026-02-04 11:04:07 +0000

    Modify the entirety of the script
    Deleted old code and began anew, to have it make more sense to me and understand Math.random() better

--- script.js
+++ script.js
@@ -1,57 +1,19 @@
-let choice1 = "scissors"
-let choice2 = "rock"
-let choice3 = "paper"
+function getComputerChoice () {
+    let scissors = "scissors";
+    let paper = "paper";
+    let rock = "rock";
 
-let computerScore = 0;
-let playerScore = 0;
-
-function getPlayerChoice() {
-    let selection = prompt("What is your choice?");
-
-    return selection;
-}
-console.log("Player choice: " + getPlayerChoice());
-
-function getComputerChoice() {
     let random = Math.floor(Math.random() * 3) + 1;
-    let selection;
 
-    switch (random) {
-        case 1:
-            selection = choice1;
-            break;
-        case 2:
-            selection = choice2;
-            break;
-        case 3:
-            selection = choice3;
-            break;
+    if (random == 1) {
+        return scissors;
     }
-
-    return selection;
-}
-
-console.log("Computer choice: " + getComputerChoice());
-
-function compareChoices() {
-    if (playerSelection == "scissors" && computerSelection == "paper") {
-        playerScore++;
-    }
-    else if (playerSelection == "rock" && computerSelection == "scissors") {
-        playerScore++;
-    }
-    else if (playerSelection == "paper" && computerSelection == "rock") {
-        playerScore++
+    else if (random == 2) {
+        return paper
     }
     else {
-        computerScore++;
+        return rock
     }
 }
 
-let playerSelection = getPlayerChoice(selection);
-let computerSelection = getComputerChoice(selection);
-
-console.log("Player Score: " + playerScore)
-console.log("Computer Score: " + computerScore)
-
-
+console.log(getComputerChoice())
